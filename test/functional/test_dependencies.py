96b499282a45f5083cf8e0c18b45492f2f2bb2c7
Miro Hrončok <miro@hroncok.cz>
2017-05-09 15:01:09 +0200

    Test a couple of RPMs

--- test/functional/test_dependencies.py
+++ test/functional/test_dependencies.py
@@ -1,4 +1,7 @@
+import glob
 import os
+
+import pytest
 
 from pyversions import two_three_check
 
@@ -7,7 +10,30 @@ def pkg(filename):
     return os.path.join(os.path.dirname(__file__), '..', 'fixtures', filename)
 
 
-def test_pyserial_depends_on_2_only():
-    name, versions = two_three_check(pkg('pyserial-2.7-6.fc25.noarch.rpm'))
-    assert name == b'pyserial'
+def gpkg(pkgglob):
+    return glob.glob(pkg(pkgglob))[0]
+
+
+@pytest.mark.parametrize('pkgglob', ('pyserial*', 'python-peak-rules*',
+                                     'python2-geoip2*'))
+def test_package_depends_on_2_only(pkgglob):
+    name, versions = two_three_check(gpkg(pkgglob))
     assert versions == {2}
+
+
+@pytest.mark.parametrize('pkgglob', ('python3-pyserial*',))
+def test_package_depends_on_3_only(pkgglob):
+    name, versions = two_three_check(gpkg(pkgglob))
+    assert versions == {3}
+
+
+@pytest.mark.parametrize('pkgglob', ('tracer*',))
+def test_package_depends_on_2_and_3(pkgglob):
+    name, versions = two_three_check(gpkg(pkgglob))
+    assert versions == {2, 3}
+
+
+@pytest.mark.parametrize('pkgglob', ('libgccjit-devel*',))
+def test_package_depends_on_no_python(pkgglob):
+    name, versions = two_three_check(gpkg(pkgglob))
+    assert not versions
