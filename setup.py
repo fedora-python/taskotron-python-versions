a53af520f36dd5fb58727967785b81e552cf075b
Iryna Shcherbina <ishcherb@redhat.com>
2017-05-18 16:30:16 +0200

    Add a setup.py so we can release on PyPI

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+
+description = """Taskotron checks regarding Python versions"""
+
+setup(
+    name='taskotron-python-versions',
+    version='0.0.dev1',
+    description=description,
+    long_description=description,
+    keywords='taskotron fedora python rpm',
+    author='Miro Hrončok, Iryna Shcherbina',
+    author_email='mhroncok@redhat.com, ishcherb@redhat.com',
+    url='https://github.com/fedora-python/task-python-versions',
+    license='Public Domain',
+    packages=find_packages(),
+    setup_requires=['setuptools', 'pytest-runner'],
+    tests_require=['pytest', 'pyyaml'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing',
+    ]
+)
