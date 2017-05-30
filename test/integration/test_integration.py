3d83bb45f1d990968438bea6ee90dde5507f3e93
Miro Hrončok <miro@hroncok.cz>
2017-05-30 21:56:47 +0200

    Convert all task results into fixtures
    This should gain some speedup

--- test/integration/test_integration.py
+++ test/integration/test_integration.py
@@ -61,56 +61,75 @@ def run_task(nevr, *, reterr=False):
     return ret
 
 
-@pytest.fixture()
-def tracer_results(_tracer_results):
-    '''This should FAIL the two_three check'''
-    print(_tracer_results[1], file=sys.stderr)
-    return _tracer_results[0]
+def fixtures_factory(nevr):
+    '''Given the NEVR and later name, create a fixture with the results
+
+    Note that this has to be called twice and assigned to correct names!
+
+    See examples bellow.'''
+    if not nevr.startswith('_'):
+        @pytest.fixture(scope="session")
+        def _results():
+            return run_task(nevr, reterr=True)
+
+        return _results
+
+    @pytest.fixture()
+    def results(request):
+        _results = request.getfixturevalue(nevr)
+        print(_results[1], file=sys.stderr)
+        return _results[0]
+
+    return results
 
 
-@pytest.fixture(scope="session")
-def _tracer_results():
-    return run_task('tracer-0.6.9-1.fc23', reterr=True)
+_tracer = fixtures_factory('tracer-0.6.9-1.fc23')
+tracer = fixtures_factory('_tracer')
+
+_copr = fixtures_factory('python-copr-1.77-1.fc26')
+copr = fixtures_factory('_copr')
+
+_eric = fixtures_factory('eric-6.1.6-2.fc25')
+eric = fixtures_factory('_eric')
+
+_six = fixtures_factory('python-six-1.10.0-3.fc25')
+six = fixtures_factory('_six')
+
+_admesh = fixtures_factory('python-admesh-0.98.5-3.fc25')
+admesh = fixtures_factory('_admesh')
+
+_epub = fixtures_factory('python-epub-0.5.2-8.fc26')
+epub = fixtures_factory('_epub')
+
+_twine = fixtures_factory('python-twine-1.8.1-3.fc26')
+twine = fixtures_factory('_twine')
 
 
-@pytest.fixture()
-def copr_results(_copr_results):
-    '''This should FAIL the name_scheme check'''
-    print(_copr_results[1], file=sys.stderr)
-    return _copr_results[0]
-
-
-@pytest.fixture(scope="session")
-def _copr_results():
-    return run_task('python-copr-1.77-1.fc26', reterr=True)
-
-
-@pytest.mark.parametrize('nevr', ('eric-6.1.6-2.fc25',
-                                  'python-six-1.10.0-3.fc25',
-                                  'python-admesh-0.98.5-3.fc25'))
-def test_two_three_nevr_passed(nevr):
-    assert run_task(nevr)['python-versions.two_three'].outcome == 'PASSED'
-
-
-def test_two_three_nevr_failed(tracer_results):
-    assert tracer_results['python-versions.two_three'].outcome == 'FAILED'
-
-
-@pytest.mark.parametrize('results', ('tracer_results', 'copr_results'))
-def test_one_failed_result_is_total_failed(results, request):
+@pytest.mark.parametrize('results', ('eric', 'six', 'admesh'))
+def test_two_three_passed(results, request):
     # getting a fixture by name
     # https://github.com/pytest-dev/pytest/issues/349#issuecomment-112203541
+    results = request.getfixturevalue(results)
+    assert results['python-versions.two_three'].outcome == 'PASSED'
+
+
+def test_two_three_failed(tracer):
+    assert tracer['python-versions.two_three'].outcome == 'FAILED'
+
+
+@pytest.mark.parametrize('results', ('tracer', 'copr'))
+def test_one_failed_result_is_total_failed(results, request):
     results = request.getfixturevalue(results)
     assert results['python-versions'].outcome == 'FAILED'
 
 
-def test_artifact_is_the_same(tracer_results):
-    assert (tracer_results['python-versions'].artifact ==
-            tracer_results['python-versions.two_three'].artifact)
+def test_artifact_is_the_same(tracer):
+    assert (tracer['python-versions'].artifact ==
+            tracer['python-versions.two_three'].artifact)
 
 
-def test_artifact_contains_two_three_and_looks_as_expected(tracer_results):
-    result = tracer_results['python-versions.two_three']
+def test_artifact_contains_two_three_and_looks_as_expected(tracer):
+    result = tracer['python-versions.two_three']
     with open(result.artifact) as f:
         artifact = f.read()
 
@@ -122,18 +141,18 @@ def test_artifact_contains_two_three_and_looks_as_expected(tracer_results):
     ''').strip().format(result.item) in artifact.strip()
 
 
-@pytest.mark.parametrize('nevr', ('eric-6.1.6-2.fc25',
-                                  'python-epub-0.5.2-8.fc26'))
-def test_naming_scheme_nevr_passed(nevr):
-    assert run_task(nevr)['python-versions.naming_scheme'].outcome == 'PASSED'
+@pytest.mark.parametrize('results', ('eric', 'epub'))
+def test_naming_scheme_passed(results, request):
+    results = request.getfixturevalue(results)
+    assert results['python-versions.naming_scheme'].outcome == 'PASSED'
 
 
-def test_naming_scheme_nevr_failed(copr_results):
-    assert copr_results['python-versions.naming_scheme'].outcome == 'FAILED'
+def test_naming_scheme_failed(copr):
+    assert copr['python-versions.naming_scheme'].outcome == 'FAILED'
 
 
-def test_artifact_contains_naming_scheme_and_looks_as_expected(copr_results):
-    result = copr_results['python-versions.naming_scheme']
+def test_artifact_contains_naming_scheme_and_looks_as_expected(copr):
+    result = copr['python-versions.naming_scheme']
     with open(result.artifact) as f:
         artifact = f.read()
 
@@ -143,21 +162,21 @@ def test_artifact_contains_naming_scheme_and_looks_as_expected(copr_results):
     """).strip().format(result.item) in artifact.strip()
 
 
-@pytest.mark.parametrize('nevr', ('eric-6.1.6-2.fc25',
-                                  'python-twine-1.8.1-3.fc26'))
-def test_requires_naming_scheme_nevr_passed(nevr):
-    task_result = run_task(nevr)['python-versions.requires_naming_scheme']
+@pytest.mark.parametrize('results', ('eric', 'twine'))
+def test_requires_naming_scheme_passed(results, request):
+    results = request.getfixturevalue(results)
+    task_result = results['python-versions.requires_naming_scheme']
     assert task_result.outcome == 'PASSED'
 
 
-def test_requires_naming_scheme_nevr_failed(copr_results):
-    task_result = copr_results['python-versions.requires_naming_scheme']
+def test_requires_naming_scheme_failed(copr):
+    task_result = copr['python-versions.requires_naming_scheme']
     assert task_result.outcome == 'FAILED'
 
 
 def test_artifact_contains_requires_naming_scheme_and_looks_as_expected(
-        tracer_results):
-    result = tracer_results['python-versions.requires_naming_scheme']
+        tracer):
+    result = tracer['python-versions.requires_naming_scheme']
     with open(result.artifact) as f:
         artifact = f.read()
 
