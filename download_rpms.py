b38c8be9697380b6e6d88ec71109b8341cf76307
Miro Hrončok <miro@hroncok.cz>
2018-02-01 20:30:59 +0100

    Fix style issues in download_rpms.py

--- download_rpms.py
+++ download_rpms.py
@@ -6,30 +6,32 @@ import sys
 import logging
 from libtaskotron.directives import koji_directive
 
+
 def download_rpms(koji_build, rpmsdir, arch=['x86_64'], arch_exclude=[],
                   src=True, debuginfo=False, build_log=True):
     '''Download RPMs for a koji build NVR.'''
 
     koji = koji_directive.KojiDirective()
 
-    print 'Downloading rpms for %s into %s' % (koji_build, rpmsdir)
-    params = {'action': 'download',
-              'koji_build': koji_build,
-              'arch': arch,
-              'arch_exclude': arch_exclude,
-              'src': src,
-              'debuginfo': debuginfo,
-              'target_dir': rpmsdir,
-              'build_log': build_log,
-             }
+    print('Downloading rpms for %s into %s' % (koji_build, rpmsdir))
+    params = {
+        'action': 'download',
+        'koji_build': koji_build,
+        'arch': arch,
+        'arch_exclude': arch_exclude,
+        'src': src,
+        'debuginfo': debuginfo,
+        'target_dir': rpmsdir,
+        'build_log': build_log,
+    }
     arg_data = {'workdir': None}
     koji.process(params, arg_data)
 
-    print 'Downloading complete'
+    print('Downloading complete')
 
 
 if __name__ == '__main__':
-    print 'Running script: %s' % sys.argv
+    print('Running script: %s' % sys.argv)
     logging.basicConfig()
     logging.getLogger('libtaskotron').setLevel(logging.DEBUG)
     args = {}
@@ -38,7 +40,7 @@ if __name__ == '__main__':
     arches = sys.argv[3] if len(sys.argv) >= 4 else ''
     arches = [arch.strip() for arch in arches.split(',')]
     if arches:
-        print 'Requested arches: %s' % arches
+        print('Requested arches: %s' % arches)
         args['arch'] = arches
 
     download_rpms(koji_build=sys.argv[1],
